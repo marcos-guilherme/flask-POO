219f858599bbf01fdec28cdb68edb47f41ff2640
marcos <marcos_oliveira2@discente.ufg.br>
2024-02-04 18:47:05 -0300

    config#5

--- models.py
+++ models.py
@@ -1,15 +1,23 @@
-from datetime import datetime
 from database import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from extensions import login_manager
+
+
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.filter_by(id=user_id)
+
+
+
 
 
 
 #Teste de criação de Models
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, autoincrement=True ,primary_key=True)
-    username = db.Column(db.String(86), nullable=False)
-    email = db.Column(db.String(86), nullable=False)
+    username = db.Column(db.String(86), nullable=False, unique=True)
+    email = db.Column(db.String(86), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
 
     def __init__(self, username, email, password):
@@ -18,6 +26,6 @@ class User(db.Model, UserMixin):
         self.password = generate_password_hash(password)
 
     def verify(self, pwd):
-        return check_password_hash(self.password, pwd)
+        return check_password_hash(self.password, pwd) 
     
 
